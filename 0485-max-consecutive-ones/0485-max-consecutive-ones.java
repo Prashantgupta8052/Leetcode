97d40ca4ab8d70504200d89cce22051d304a8b58
Prashantgupta8052 <pg6488567@gmail.com>
2026-09-26 19:38:22 +0530

    Time: 2 ms (98.24%), Space: 52.6 MB (46.07%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -3,16 +3,18 @@ class Solution {
         int n=nums.length;
         int count =0; 
         int max=0;
-        for(int i=0; i<n; i++){
-            if(nums[i]==1){
+        int j=0;
+        while(j<n){
+            if(nums[j]==1){
                 count++;
-                max=Math.max(max,count);
             }
             else{
+                max=Math.max(max,count);
                 count=0;
             }
+            j++;
 
         }
-        return max;
+        return Math.max(max,count) ;
     }
 }
